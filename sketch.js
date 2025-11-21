43c59616b70a60a7d01bb8dc840b06cb60dd480a
James Sevinc <james.sevin@pioneeracademy.org>
2025-11-21 13:33:33 -0500

    snowflakeIMages

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
   createCanvas(400, 400);
   for(i=0;i<ballCount;i++){
     collection.push({
-    size: random(3,10),
+    size: random(30),
     x: random(0,width),
     y: random(0,100),
     speed: random(1,5)                    
@@ -12,12 +12,28 @@ function setup() {
 
   }
 }
+var snowflake1 = 0
+var snowflake2 = 0
+var snowflake3 = 0
+function preload (){
+  snowflake1 = loadImage("Images/snowflake0.png")
+  snowflake2 = loadImage("Images/snowflake1.png")
+  snowflake3 = loadImage("Images/snowflake2.png")
+}
 
 function draw() {
 background(220);
 for(j=0; j<collection.length;j++){
   collection[j].y += collection[j].speed
-  ellipse(collection[j].x,collection[j].y,collection[j].size)
+  if(j%2==0){
+  image(snowflake1,collection[j].x,collection[j].y,collection[j].size,collection[j].size)
+  }
+  if(j%3==0){
+  image(snowflake2,collection[j].x,collection[j].y,collection[j].size,collection[j].size)
+  }
+  else{
+  image(snowflake3,collection[j].x,collection[j].y,collection[j].size,collection[j].size)
+  }
   if(collection[j].y>=400){
   collection[j].y = 0
   collection[j].x = random(0,width)
